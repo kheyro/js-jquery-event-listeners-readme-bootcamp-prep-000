c770244a962a94c2cb86924f8a7aa76e5467287e
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 15:50:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,6 +19,13 @@ function pressIt() {
   });
 }
 
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('your form is going to be submitted now');
+    return;
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
