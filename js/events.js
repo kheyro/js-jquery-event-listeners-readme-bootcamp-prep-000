881375bd1430a5ede5778fd3e4a833b49b339aa1
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 15:45:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,20 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $(window).on("load", function() {
+    $('img').addClass('tasty');
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
+
+
 
 });
